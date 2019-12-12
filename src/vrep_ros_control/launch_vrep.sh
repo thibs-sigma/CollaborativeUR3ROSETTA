60c057e6eda8dd4b9fb8d29742b49674095f3159
thib <thibaud.hiltenbrand@sigma-clermont.fr>
2019-12-12 14:24:22 -0300

    Updates

--- src/vrep_ros_control/launch_vrep.sh
+++ src/vrep_ros_control/launch_vrep.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# COPPELIASIM V. 4.0.0
+#cd $COPPELIASIM_ROOT && 
+#./coppeliaSim.sh -s /home/thib/CollaborativeUR3ROSETTA/ROSETTA_Project_CLEAN_v2.ttt
+
+# V-REP 3.6.2
 cd $VREP_ROOT && 
 ./vrep.sh -s /home/thib/CollaborativeUR3ROSETTA/ROSETTA_Project_CLEAN_v2.ttt
